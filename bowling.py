0ff8cffd92728583fc8829fbb3b9529c22f95a5c
kebarr <katarzinia@gmail.com>
2014-04-22 22:03:48 +0100

    1st test
    gutter game test passes

--- bowling.py
+++ bowling.py
@@ -1,9 +1,8 @@
 # use strategy james suggested: have scorecard as input then return it.
 # to start with, have 1d list 
 
-
+# actually to pass gutter game, do bare minimum
 def roll_ball(frame, pins):
-    pass
+    return frame + pins
 
-def score(frame):
-    pass
+
--- bowling_tests.py
+++ bowling_tests.py
@@ -1,14 +1,15 @@
 import unittest
-from bowling import roll_ball, score
+from bowling import roll_ball
+
 
 class PythonBowlingGame(unittest.TestCase):
     def setUp(self):
-        self.frame = []
+        self.frame = 0
 
     def test_gutter_game(self):
         for i in range(20):
             self.frame = roll_ball(self.frame, 0)
-        self.assertEqual(0, score(self.frame))
+        self.assertEqual(0, self.frame)
 
 
 if __name__ == '__main__':
