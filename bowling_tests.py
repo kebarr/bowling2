2955cf4899c712ca0cd98d1d84c9337b16e9f069
kebarr <katarzinia@gmail.com>
2014-04-22 22:05:12 +0100

    2nd test
    works same as gutter game

--- bowling_tests.py
+++ bowling_tests.py
@@ -11,6 +11,11 @@ class PythonBowlingGame(unittest.TestCase):
             self.frame = roll_ball(self.frame, 0)
         self.assertEqual(0, self.frame)
 
+    def test_all_ones(self):
+        for i in range(20):
+            self.frame = roll_ball(self.frame, 1)
+        self.assertEqual(20, self.frame)
+
 
 if __name__ == '__main__':
     unittest.main()
